fd97760ae2f7b01f7d70a65f3592c69d4ae6668d
Beth <144475715+Bpowell55@users.noreply.github.com>
2023-12-04 16:46:29 -0500

    lastquiz

---
+++ quizzes/quiz03/main.js
@@ -0,0 +1,32 @@
+
+const color = ['red', 'blue', 'yellow', 'green', 'pink', 'purple'];
+
+   
+function color1() {
+    // target the element with the id of square1
+    // and change its background color...
+    document.onclick
+}
+
+
+
+function color2() {
+    // target the element with the id of square2
+    // and change its background color...
+}
+
+function color3() {
+    // TODO
+}
+
+function color4() {
+    // TODO
+}
+
+function color5() {
+    // TODO
+}
+
+function color6() {
+    // TODO
+}
