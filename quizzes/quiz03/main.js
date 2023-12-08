bf5736daa254bf9e1aa41d46822640fabd5b829f
Beth <144475715+Bpowell55@users.noreply.github.com>
2023-12-08 11:09:46 -0500

    Quiz3WORKING

--- quizzes/quiz03/main.js
+++ quizzes/quiz03/main.js
@@ -5,28 +5,34 @@ const color = ['red', 'blue', 'yellow', 'green', 'pink', 'purple'];
 function color1() {
     // target the element with the id of square1
     // and change its background color...
-    document.onclick
+    document.getElementById("square1").style.backgroundColor = "red";
 }
 
 
 
 function color2() {
+
     // target the element with the id of square2
     // and change its background color...
+    document.getElementById("square2").style.backgroundColor = color[2];
 }
 
 function color3() {
     // TODO
+    document.getElementById("square3").style.backgroundColor = color[3];
 }
 
 function color4() {
     // TODO
+    document.getElementById("square4").style.backgroundColor = color[4];
 }
 
 function color5() {
     // TODO
+    document.getElementById("square5").style.backgroundColor = color[5];
 }
 
 function color6() {
+    document.getElementById("square6").style.backgroundColor = color[2];
     // TODO
 }
